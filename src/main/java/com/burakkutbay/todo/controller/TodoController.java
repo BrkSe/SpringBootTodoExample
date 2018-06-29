0189793a561ffcda438084d97ee6c7b323138080
BrkSe <brkktby@gmail.com>
2018-06-30 00:24:43 +0300

    index requestmapping

--- src/main/java/com/burakkutbay/todo/controller/TodoController.java
+++ src/main/java/com/burakkutbay/todo/controller/TodoController.java
@@ -1,4 +1,25 @@
 package com.burakkutbay.todo.controller;
 
+import com.burakkutbay.todo.entity.Todo;
+import com.burakkutbay.todo.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+@Controller
 public class TodoController {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @RequestMapping("/")
+    public String index(Model model){
+        ArrayList todoList= (ArrayList) todoRepository.findAll();
+        model.addAttribute("newtodo", new Todo());
+        model.addAttribute("todos", todoList);
+        return "index";
+    }
 }
