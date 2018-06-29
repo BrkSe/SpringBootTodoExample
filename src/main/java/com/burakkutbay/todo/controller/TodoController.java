ad6dc53afe20bdf508e9b50e85c255e3046e70d2
BrkSe <brkktby@gmail.com>
2018-06-30 00:28:59 +0300

    add requestmapping added

--- src/main/java/com/burakkutbay/todo/controller/TodoController.java
+++ src/main/java/com/burakkutbay/todo/controller/TodoController.java
@@ -5,6 +5,7 @@ import com.burakkutbay.todo.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -16,10 +17,17 @@ public class TodoController {
     private TodoRepository todoRepository;
 
     @RequestMapping("/")
-    public String index(Model model){
-        ArrayList todoList= (ArrayList) todoRepository.findAll();
+    public String index(Model model) {
+        ArrayList todoList = (ArrayList) todoRepository.findAll();
         model.addAttribute("newtodo", new Todo());
         model.addAttribute("todos", todoList);
         return "index";
+    }
+
+    @RequestMapping("/add")
+    public String addTodo(@ModelAttribute Todo todo) {
+        Todo todo1 = new Todo(todo.getTitle(), todo.getTitle());
+        todoRepository.save(todo1);
+        return "redirect:/";
     }
 }
