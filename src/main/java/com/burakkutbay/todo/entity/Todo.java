8755483c08e34ca22fc4bacbb857f8a196511e1e
BrkSe <brkktby@gmail.com>
2018-06-29 22:42:37 +0300

    Todo variable added.

--- src/main/java/com/burakkutbay/todo/entity/Todo.java
+++ src/main/java/com/burakkutbay/todo/entity/Todo.java
@@ -1,7 +1,69 @@
 package com.burakkutbay.todo.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String detail;
+    private boolean complete;
+
+    public Todo() {
+    }
+
+    public Todo(String title, String detail, boolean complete) {
+        this.title = title;
+        this.detail = detail;
+        this.complete = complete;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", detail='" + detail + '\'' +
+                ", complete=" + complete +
+                '}';
+    }
 }
