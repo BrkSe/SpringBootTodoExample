0213a347a18e2af7ec95f1ba9d29aab38760dc63
BrkSe <brkktby@gmail.com>
2018-07-04 13:24:43 +0300

    percent calcutor and update

--- src/main/java/com/burakkutbay/todo/controller/TodoController.java
+++ src/main/java/com/burakkutbay/todo/controller/TodoController.java
@@ -2,6 +2,7 @@ package com.burakkutbay.todo.controller;
 
 import com.burakkutbay.todo.entity.Todo;
 import com.burakkutbay.todo.repository.TodoRepository;
+import com.burakkutbay.todo.util.Calculator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -20,35 +21,71 @@ public class TodoController {
     @Autowired
     private TodoRepository todoRepository;
 
-    @RequestMapping("/")
+    private Calculator calculator=new Calculator();
+
+    @Valid
+    private List<Todo> todoList;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model) {
-        List<Todo> todoList = (List<Todo>) todoRepository.findAll();
+        todoList = (List<Todo>) todoRepository.findAll();
+        int completedTask=0;
+        for (Todo todo: todoList){
+            if(todo.isComplete()){
+                completedTask++;
+            }
+        }
+        String a=calculator.percentTask(todoList.size(),completedTask);
+
         model.addAttribute("newtodo", new Todo());
         model.addAttribute("todos", todoList);
+        model.addAttribute("percentTask",a);
         return "index";
 
     }
 
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String addTodo(@ModelAttribute Todo todo, Model model,HttpServletRequest request) {
+    //@RequestMapping(value = "/add", method = RequestMethod.POST)
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String addTodo(@ModelAttribute Todo todo, Model model, HttpServletRequest request) {
         todoRepository.save(todo);
         return "redirect:/";
     }
 
+//    @RequestMapping(value = "/update",method = RequestMethod.POST)
+//    public String updateTodo(@ModelAttribute("todos") ArrayList<Todo> todoList) {
+//         for (Todo todo1 : todoList) {
+//            Todo todo = new Todo(todo1.getDetail(), todo1.getTitle());
+//            todo.setComplete(todo1.isComplete());
+//            todo.setId(todo1.getId());
+//            todoRepository.save(todo);
+//        }
+//        return "redirect:/";
+//    }
+
     @RequestMapping("/update")
-    public String updateTodo(@ModelAttribute ArrayList<Todo> todoList) {
-        for (Todo todo1 : todoList) {
-            Todo todo = new Todo(todo1.getDetail(), todo1.getTitle());
-            todo.setComplete(todo1.isComplete());
-            todo.setId(todo1.getId());
-            todoRepository.save(todo);
+    public String updateTodo(@RequestParam("id") Long id) {
+        Todo todo = todoRepository.findById(id).get();
+        if (todo.isComplete()) {
+            todo.setComplete(false);
+        } else {
+            todo.setComplete(true);
         }
+        todoRepository.save(todo);
         return "redirect:/";
     }
+
 
     @RequestMapping("/delete")
     public String deleteTodo(@RequestParam("id") Long id) {
         todoRepository.deleteById(id);
         return "redirect:/";
+    }
+
+    public List<Todo> getTodoList() {
+        return todoList;
+    }
+
+    public void setTodoList(List<Todo> todoList) {
+        this.todoList = todoList;
     }
 }
